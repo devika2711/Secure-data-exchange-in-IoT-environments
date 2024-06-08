2a2448a215e0b888b1627c85c1e85270e1eac9c7
Devika Makan <108333894+devika2711@users.noreply.github.com>
2024-06-08 20:52:20 +0530

    Update AES.java

--- Edge Server/AES.java
+++ Edge Server/AES.java
@@ -1,1 +1,30 @@
-
+package com;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Arrays;
+public class AES {
+	
+	public static byte[] encrypt(byte input[],byte keys[])throws Exception{
+		byte akey[] = new byte[16];
+		for(int i=0;i<16;i++){
+			akey[i] = keys[i];
+		}
+		SecretKeySpec key = new SecretKeySpec(akey,0,akey.length,"AES");
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+	    byte encbytes[] = cipher.doFinal(input);
+		return encbytes;
+	}
+	public static byte[] decrypt(byte[] enc,byte keys[])throws Exception {
+		byte akey[] = new byte[16];
+		for(int i=0;i<16;i++){
+			akey[i] = keys[i];
+		}
+		SecretKeySpec key = new SecretKeySpec(akey,0,akey.length,"AES");
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte decbytes[] = cipher.doFinal(enc);
+		return decbytes;
+	}
+	
+}         
